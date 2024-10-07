f3eb0a860a01a2d2c789fb87a5c689d5b7352313
Melissa Wang <melissa.wang21@gmail.com>
2024-10-07 14:13:08 -0400

    loops complete

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    for i in range(10):
+        print(10 - i)
+    print("Happy New Year!")
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    tmp = [x**2 for x in int_list]
+    return tmp
 
 def fizzbuzz():
-    # code goes here!
+    for i in range (100):
+        n = i + 1
+        if (n % 3 == 0):
+            if (n % 5 == 0):
+                print("FizzBuzz")
+            else:
+                print("Fizz")
+        elif (n % 5 == 0):
+            print("Buzz")
+        else:
+            print(n)
+        
     pass
